3f4e56d36586817f06b3cad3d1c58026f89f5ecb
HangCui <oscarcuihang@hotmail.com>
2014-12-08 21:54:50 -0600

    upload

---
+++ map_bootstrap/page/index.php
@@ -0,0 +1,19 @@
+<?php
+	include ('../template/header.html');
+?>
+	<title></title>
+
+  	
+</head>
+<body>
+	
+<?php
+	include ('../template/navbar.html');
+?>
+
+
+
+<?php
+	include ('../template/copyrights.html');
+	include ('../template/footer.html');
+?>
